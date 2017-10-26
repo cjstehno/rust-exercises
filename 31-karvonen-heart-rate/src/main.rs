05cfef1dca390eb7d608f1eb025f38af676af699
stehnoc <chris.stehno@netscout.com>
2017-10-26 11:20:13 -0500

    added docs

--- 31-karvonen-heart-rate/src/main.rs
+++ 31-karvonen-heart-rate/src/main.rs
@@ -5,6 +5,17 @@ use clap::{Arg, App};
 use console::Term;
 use std::process::exit;
 
+///
+/// Takes input of the age and resting heart rate and generates a table of target heart rates.
+///
+/// Optional Command Line Arguments:
+/// * age (`-a`, `--age`)
+/// * resting heart rate (`-r`, `--resting`)
+/// * intensity (`-i`, `--intensity`)
+///
+/// Note: if the `intensity` is specified, only that value will be returned, otherwise the whole table
+/// will be generated.
+///
 fn main() {
     let matches = App::new("karvonen-heart-rate")
         .version("0.0.1")
@@ -44,12 +55,9 @@ fn main() {
     }
 }
 
+/// Parses the input from the command line or prompt and returns a number value.
 fn number_input(term: &Term, arg: Option<&str>, label: &str) -> u16 {
-    parse_input(term, resolve_input(term, arg, label), label)
-}
-
-fn resolve_input(term: &Term, arg: Option<&str>, label: &str) -> String {
-    match arg {
+    let input = match arg {
         Some(value) => String::from(value),
         None => {
             term.write_str(&format!("What is your {}? ", label)).unwrap();
@@ -61,10 +69,8 @@ fn resolve_input(term: &Term, arg: Option<&str>, label: &str) -> String {
                 exit(0);
             }
         }
-    }
-}
+    };
 
-fn parse_input(term: &Term, input: String, label: &str) -> u16 {
     match input.parse::<u16>() {
         Ok(num) => num,
         Err(_) => {
@@ -74,6 +80,7 @@ fn parse_input(term: &Term, input: String, label: &str) -> u16 {
     }
 }
 
+/// Calculates the Karvonen Heart Rate value.
 fn calculate_target_rate(age: u16, resting_rate: u16, intensity: u16) -> u16 {
     (((220 - age) - resting_rate) * intensity / 100) + resting_rate
 }
