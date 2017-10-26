ccb3ed98982d02f807563eb5df687d28703c91d1
stehnoc <chris.stehno@netscout.com>
2017-10-26 10:42:22 -0500

    added basic cli support

--- 31-karvonen-heart-rate/src/main.rs
+++ 31-karvonen-heart-rate/src/main.rs
@@ -1,20 +1,62 @@
 extern crate console;
+extern crate clap;
 
+use clap::{Arg, App, SubCommand};
 use console::Term;
 use std::process::exit;
 
-// TODO: make version that takes cli args or presents with questions
-
 fn main() {
+    let matches = App::new("karvonen-heart-rate")
+        .version("0.0.1")
+        .author("Christopher J. Stehno <chris@stehno.com>")
+        .about("Calculates your target heart rate given age and resting heart rate.")
+        .arg(Arg::with_name("age")
+            .short("a")
+            .long("age")
+            .value_name("AGE")
+            .help("Specifies the age.")
+            .takes_value(true))
+        .arg(Arg::with_name("resting")
+            .short("r")
+            .long("resting")
+            .value_name("RATE")
+            .help("Specifies the resting heart rate.")
+            .takes_value(true))
+        .get_matches();
+
     let term = Term::stdout();
 
-    term.write_str("How old are you? ").unwrap();
+    let mut age: u16 = 0;
 
-    let age: u16 = parse_number_input(&term, "age");
+    if let Some(value) = matches.value_of("age") {
+        match value.parse::<u16>() {
+            Ok(num) => age = num,
+            Err(_) => {
+                term.write_line("A valid age must be provided.").unwrap();
+                exit(0);
+            }
+        }
+    } else {
+        term.write_str("How old are you? ").unwrap();
 
-    term.write_str("What is your resting heart rate? ").unwrap();
+        age = parse_number_input(&term, "age");
+    }
 
-    let resting_heart_rate: u16 = parse_number_input(&term, "resting heart rate");
+    let mut resting_heart_rate: u16 = 0;
+
+    if let Some(value) = matches.value_of("resting") {
+        match value.parse::<u16>() {
+            Ok(num) => resting_heart_rate = num,
+            Err(_) => {
+                term.write_line("A valid resting heart rate must be provided.").unwrap();
+                exit(0);
+            }
+        }
+    } else {
+        term.write_str("What is your resting heart rate? ").unwrap();
+
+        resting_heart_rate = parse_number_input(&term, "resting heart rate");
+    }
 
     term.write_line(&format!("\nResting Pulse: {}  Age: {}\n", resting_heart_rate, age)).unwrap();
 
